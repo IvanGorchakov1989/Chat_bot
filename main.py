da7f47411e38e2fecd0e63297f575340b7e50417
Gorchakov Ivan <madow@mail.ru>
2024-02-12 19:20:12 +0300

    add

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-import math
 import requests
-from pprint import pprint
+import time
+from config import *
 
-api_url = 'http://api.open-notify.org/iss-now.json'
 
-response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
 
-if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
-    pprint(response.text)
-else:
-    pprint(response.status_code)    # При другом коде ответа выводим этот код
+offset = -2
+counter = 0
+chat_id: int
+
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
